a7e21ddea1a31fb5fa7a8d8d56bc3b1423462135
hyxx-su <legendary9899@dgsw.hs.kr>
2026-01-22 14:12:12 +0900

    delete : fonts and colors

--- src/constants/style/theme.ts
+++
@@ -1,9 +0,0 @@
-import { Colors } from './Colors';
-import { Typography } from './Fonts'
-
-export const theme = {
-  colors: Colors,
-  typography: Typography,
-} as const;
-
-export type Theme = typeof theme;
