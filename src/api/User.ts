a7693f11dc91827e2a3002ee0142cdb83d225357
hyxx-su <legendary9899@dgsw.hs.kr>
2026-03-04 15:05:07 +0900

    feat : user api 연결

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,11 +1,18 @@
 import type { MyPost } from '@/types/post';
 import instance from './Axios';
-import type { User } from '@/types/user';
+import type { User, UpdateProfileRequest } from '@/types/user';
 
 // 사용자 정보 가져오기
 export const getUser = async (): Promise<User> => {
   const response = await instance.get<User>('/me');
   return response.data;
+};
+
+// 프로필 수정
+export const updateProfile = async (
+  data: UpdateProfileRequest,
+): Promise<void> => {
+  await instance.put('/me/profile', data);
 };
 
 // 사용자가 작성한 글 가져오기
@@ -16,12 +23,12 @@ export const getMyPost = async (): Promise<MyPost> => {
 
 // 사용자가 댓글 단 글 가져오기
 export const getCommentedPost = async (): Promise<MyPost> => {
-  const response = await instance.get<MyPost>('/me/commented-posts');
+  const response = await instance.get<MyPost>('/me/comments');
   return response.data;
 };
 
 // 사용자가 좋아요한 글 가져오기
 export const getLikedPost = async (): Promise<MyPost> => {
-  const response = await instance.get<MyPost>('/me/liked-posts');
+  const response = await instance.get<MyPost>('/me/hearts');
   return response.data;
 };
