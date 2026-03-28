e0768bbebb265cec573f223d6dc2ac65f076e9b1
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-03-28 18:15:16 +0900

    fix : ESlint 에러 수정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -35,7 +35,7 @@ function App() {
     });
 
     return () => cleanup();
-  }, [isLoggedIn]);
+  }, [isLoggedIn, fetchUser]);
 
   return (
     <ThemeProvider theme={theme}>
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,5 +2,5 @@ import 'styled-components';
 import type { Theme } from '@/styles/Theme';
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
+  type DefaultTheme = Theme;
 }
