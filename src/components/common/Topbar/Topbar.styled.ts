408171d38a4d593cdbcb44ccb90bc042d716336e
hyxx-su <legendary9899@dgsw.hs.kr>
2026-04-08 20:15:09 +0900

    refactor : 주석 제거

--- src/components/common/Topbar/Topbar.styled.ts
+++ src/components/common/Topbar/Topbar.styled.ts
@@ -100,10 +100,6 @@ export const Nav = styled.nav`
   @media screen and (max-width: 1200px) {
     gap: 4rem;
   }
-  /* 
-  @media screen and (max-width: 1024px) {
-    gap: 2.5rem;
-  } */
 
   @media screen and (max-width: 1024px) {
     display: none;
