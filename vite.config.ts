851e2922f34c9ec97ae9b361a70f7e2457e9a36b
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-04-13 20:49:29 +0900

    refactor : lighthouse 렌더링 차단 요청 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    cssCodeSplit: false, 
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
 });
